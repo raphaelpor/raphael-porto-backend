b46da6432082d67c2c262e22d4a816611e956e49
Raphael Porto <raphael.freitas@corp.globo.com>
2019-10-06 18:40:25 -0300

    Improve lint/flow/prettie integration

--- src/helpers/requestWrapper.js
+++ src/helpers/requestWrapper.js
@@ -2,8 +2,8 @@
 const http = require('http');
 const https = require('https');
 
-function requestWrapper(uri: string) {
-  const requestHandler = uri?.includes('https') ? https : http;
+function requestWrapper(uri: string): http$ClientRequest<any> {
+  const requestHandler = uri.includes('https') ? https : http;
 
   return requestHandler.get(uri);
 }
